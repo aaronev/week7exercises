565f180c9509cab972b044be5ca6d217e230bdae
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-18 21:08:55 -0700

    completes exericse 9

--- js/index.js
+++ js/index.js
@@ -2,5 +2,6 @@ document.getElementById('pracH1').textContent= "What is it!"
 
 function redBody(){
   var val = document.getElementById('pracInp').value
+  document.getElementById('pracH12').textContent = val
   console.log(val)
 }
